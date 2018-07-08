cc7e4a67161aab7cc9c36f3bc1487d7c635c6d43
EricHodgins <hodgins.e@gmail.com>
2018-07-08 18:56:25 -0400

    completed RMSE in tools.

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,18 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+	VectorXd rmse = VectorXd(estimations[0].size());
+	rmse.fill(0.0);
+
+	VectorXd diff = VectorXd(estimations[0].size());
+	for (int i = 0; i < estimations.size(); i++) {
+		diff = estimations[i] - ground_truth[i];
+		diff = diff.array() * diff.array();
+
+		rmse = rmse + diff;
+	}
+
+	rmse = rmse / estimations.size();
+
+	rmse = rmse.array().sqrt();
 }
